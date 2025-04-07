e7219130c6ce1281405cff9c0a52049230fde3cb
nishantdania <nishantdania@gmail.com>
2025-04-07 20:18:46 +0800

    Add a title
    So that browser bookmarks are easier to read.

--- main.go
+++ main.go
@@ -158,6 +158,7 @@ func main() {
 		w.Write([]byte(`
 			<html>
 			<head>
+				<title>Notes</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 				<script>
 					// Debounce function to limit how often autosave is triggered
